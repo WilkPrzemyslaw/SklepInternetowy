e7b5b69d890a288efb8c0b37e21f935a66107c7c
Przemek <Przemek@GRONLAND>
2017-08-16 22:11:21 +0200

    Add project files.

---
+++ SklepInternetowy/Models/PanelLogowania.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepInternetowy.Models
+{
+    public class PanelLogowania
+    {
+        private string _login = "admin";
+        private string _haslo = "1234";
+        public string login;
+        public string haslo;
+
+        public PanelLogowania()
+        {
+
+        }
+        public PanelLogowania(string l, string h)
+        {
+            login = l;
+            haslo = h;
+        }
+        public void podajLoginHaslo()
+        {
+            Console.Clear();
+            Console.WriteLine("Podaj login:");
+            login = Console.ReadLine();
+            Console.WriteLine("Podaj haslo:");
+            haslo = Console.ReadLine();
+
+            sprawdzLoginHaslo();
+        }
+
+        public void sprawdzLoginHaslo()
+        {
+           
+            
+            if (login == _login && haslo == _haslo)
+            {
+                Console.WriteLine("witaj");
+            }
+            else
+            {
+                Console.WriteLine("blednie podane haslo sprobuj jeszcze raz");
+                Console.ReadKey();
+                podajLoginHaslo();
+            }
+        }
+    }
+}
---
+++ SklepInternetowy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepInternetowy
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+
+            menu();
+
+        }
+
+        static public void menu()
+        {
+            Console.Title = "Menu";
+
+            Models.PanelLogowania pl = new Models.PanelLogowania();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Witaj w sklepie internetowym:");
+                Console.WriteLine("1. Przeglad");
+                Console.WriteLine("2. Zaloguj");
+                Console.WriteLine("3. Wyjdz");
+
+                ConsoleKeyInfo klawisz = Console.ReadKey();
+                Console.WriteLine();
+                switch (klawisz.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear(); break;
+                    case ConsoleKey.D2:
+                        Console.Clear(); pl.podajLoginHaslo(); Console.ReadKey(); break;
+                    case ConsoleKey.D3:
+                        Environment.Exit(0); break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
